92917b50c8aa667cf3437a44350579226c2dd717
shlampley <shanelampley09@gmail.com>
2021-06-08 21:39:00 -0700

    new changes to tic tac toe

--- programs/tic_tac_toe.py
+++ programs/tic_tac_toe.py
@@ -7,17 +7,22 @@ class Board:
     def __init__(self):
         self.board = ""    
     def __str__(self):
-        return self.draw()
+        return self.get_board()
     def __repr__(self):
         return "this is a board object"    
     
-    def draw(self):
+    def get_board(self):
         x = 0
         self.board = f' ____________________\n|      |      |      |\n|  {x}   |  {x+1}   |  {x+2}   |\n|______|______|______|\n|      |      |      |\n|  {x+3}   |  {x+4}   |  {x+5}   |\n|______|______|______|\n|      |      |      |\n|  {x+6}   |  {x+7}   |  {x+8}   |\n|______|______|______|'
         return self.board
+    def get_base_board(self):
+        x = 0
+        return f' ____________________\n|      |      |      |\n|  {x}   |  {x+1}   |  {x+2}   |\n|______|______|______|\n|      |      |      |\n|  {x+3}   |  {x+4}   |  {x+5}   |\n|______|______|______|\n|      |      |      |\n|  {x+6}   |  {x+7}   |  {x+8}   |\n|______|______|______|'
+def user_inputs(x1.get_base_board):
+    print(x1.get_base_board)
 x1 = Board()
-x1.draw()
-print(x1)
+x1.get_board()
+user_inputs(x1)
 # class BoardTile(Board):
 #     def __init__(self, player_marker, coordinates):
 #         self.player_marker = player_marker
