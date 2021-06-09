f22a1aae442aae5adbcd353e437ec55c33165cea
shlampley <shanelampley09@gmail.com>
2021-06-08 22:52:12 -0700

    Co-authored-by: Austin Benitez <Davisa94@users.noreply.github.com>

--- programs/tic_tac_toe.py
+++ programs/tic_tac_toe.py
@@ -1,3 +1,5 @@
+from itertools import cycle
+
 class Players:
     def __init__(self, name, solo, multi):
         self.name = input("name")
@@ -5,24 +7,53 @@ class Players:
         
 class Board:
     def __init__(self):
-        self.board = ""    
+        self.board = ""
+        self.moves = self.set_empty_moves()
+        self.turns = ["X", "O"]
+        self.player_turn = self.get_player_iterator()
     def __str__(self):
         return self.get_board()
     def __repr__(self):
         return "this is a board object"    
     
+    def set_empty_moves(self):
+        return [x for x in range(9)]
+        
     def get_board(self):
-        x = 0
-        self.board = f' ____________________\n|      |      |      |\n|  {x}   |  {x+1}   |  {x+2}   |\n|______|______|______|\n|      |      |      |\n|  {x+3}   |  {x+4}   |  {x+5}   |\n|______|______|______|\n|      |      |      |\n|  {x+6}   |  {x+7}   |  {x+8}   |\n|______|______|______|'
+        
+        self.board = f' ____________________\n|      |      |      |\n|  {self.moves[0]}   |  {self.moves[1]}   |  {self.moves[2]}   |\n|______|______|______|\n|      |      |      |\n|  {self.moves[3]}   |  {self.moves[4]}   |  {self.moves[5]}   |\n|______|______|______|\n|      |      |      |\n|  {self.moves[6]}   |  {self.moves[7]}   |  {self.moves[8]}   |\n|______|______|______|'
         return self.board
-    def get_base_board(self):
-        x = 0
-        return f' ____________________\n|      |      |      |\n|  {x}   |  {x+1}   |  {x+2}   |\n|______|______|______|\n|      |      |      |\n|  {x+3}   |  {x+4}   |  {x+5}   |\n|______|______|______|\n|      |      |      |\n|  {x+6}   |  {x+7}   |  {x+8}   |\n|______|______|______|'
-def user_inputs(x1.get_base_board):
-    print(x1.get_base_board)
-x1 = Board()
-x1.get_board()
-user_inputs(x1)
+    # def get_base_board(self):
+    #     x = 0
+    #     return f' ____________________\n|      |      |      |\n|  {x}   |  {x+1}   |  {x+2}   |\n|______|______|______|\n|      |      |      |\n|  {x+3}   |  {x+4}   |  {x+5}   |\n|______|______|______|\n|      |      |      |\n|  {x+6}   |  {x+7}   |  {x+8}   |\n|______|______|______|'
+
+    # Used to get the current turn
+    def get_player_iterator(self):
+        return cycle(self.turns)
+
+
+# Rec
+def user_inputs(board):
+    curr_turn = next(board.player_turn)
+    #Move the line below me into the main game loop and instead
+    print(board)
+    choice = input(f"{curr_turn} where do you want to move 0-8: ")
+    board.moves[int(choice)] = curr_turn
+    return board # returns board with curent player selection
+
+
+def game():
+    # We need to make a new board
+    x1 = Board()
+    # We need to display the current board state
+    # we need to get the users choice of where to place their move
+    for x in x1.moves:
+        x1 = user_inputs(x1) # need to store value!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # repeat until????
+    # print final game state
+    print(x1)
+game()
+
 # class BoardTile(Board):
 #     def __init__(self, player_marker, coordinates):
 #         self.player_marker = player_marker
