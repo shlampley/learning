86e715faa43c5d7f85788d4befab78bf7971806e
shlampley <shanelampley09@gmail.com>
2021-06-09 20:17:13 -0700

    trying to set win condition

--- programs/tic_tac_toe.py
+++ programs/tic_tac_toe.py
@@ -11,6 +11,7 @@ class Board:
         self.moves = self.set_empty_moves()
         self.turns = ["X", "O"]
         self.player_turn = self.get_player_iterator()
+        self.win_board() =
     def __str__(self):
         return self.get_board()
     def __repr__(self):
@@ -23,7 +24,7 @@ class Board:
         
         self.board = f' ____________________\n|      |      |      |\n|  {self.moves[0]}   |  {self.moves[1]}   |  {self.moves[2]}   |\n|______|______|______|\n|      |      |      |\n|  {self.moves[3]}   |  {self.moves[4]}   |  {self.moves[5]}   |\n|______|______|______|\n|      |      |      |\n|  {self.moves[6]}   |  {self.moves[7]}   |  {self.moves[8]}   |\n|______|______|______|'
         return self.board
-    # def get_base_board(self):
+    #  def get_base_board(self):
     #     x = 0
     #     return f' ____________________\n|      |      |      |\n|  {x}   |  {x+1}   |  {x+2}   |\n|______|______|______|\n|      |      |      |\n|  {x+3}   |  {x+4}   |  {x+5}   |\n|______|______|______|\n|      |      |      |\n|  {x+6}   |  {x+7}   |  {x+8}   |\n|______|______|______|'
 
@@ -52,6 +53,11 @@ def game():
     # repeat until????
     # print final game state
     print(x1)
+    while True:
+        if x1 == win_board:
+            print(f"{next} YOU WIN!!!! ")
+        else:
+            print(f"{next} YOU WIN!!!! ")
 game()
 
 # class BoardTile(Board):
