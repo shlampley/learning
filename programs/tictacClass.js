efbe0f2b12d1b078d9320ffac1a3e81c649c8496
Davisa94 <Austin.i.davis94@gmail.com>
2021-06-11 15:14:48 -0700

    Added check diagonal game function

--- programs/tictacClass.js
+++ programs/tictacClass.js
@@ -57,3 +57,37 @@ let Game = {
         }
     },
 };
+
+function checkDiagonals() {
+    //get diagonal
+    //grab every 4th cellstarting t the first cell
+    //check if they are all equal
+    let diagonals = [];
+    let won = false;
+    for (let i = 0; i < 9; i++) {
+        if (i % 4 == 0) {
+            diagonals.push(cells[i].innerHTML)
+                //console.log("Checking diagonals" + " " + i + " " + diagonals[i % 3])
+
+        }
+    }
+    if (diagonals.every(val => val === diagonals[0] && val != '')) {
+        console.log("game won on diagonal" + diagonals.length);
+        return true;
+    }
+    let opDiagonals = [];
+    //get opposite diagonals, get every other cell starting at the third cell and ending on the 7th
+    //check if they are all equal
+    for (let i = 2; i <= 6; i++) {
+        if (i % 2 == 0) {
+            opDiagonals.push(cells[i].innerHTML) //cells[i]
+                // console.log("Checking opposite diagonals" + " " + i + " " + opDiagonals[i % 3])
+        }
+    }
+
+    if (opDiagonals.every(val => val === opDiagonals[0] && val != '')) {
+        console.log("game won on opposite diagonal");
+        return true;
+    }
+    return false;
+}
