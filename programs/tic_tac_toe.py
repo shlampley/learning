05d83957976a845b5f0d04a1bde3529117a49e52
shlampley <shanelampley09@gmail.com>
2021-06-18 23:45:42 -0700

    Co-authored-by: Austin Benitez <Davisa94@users.noreply.github.com>

--- programs/tic_tac_toe.py
+++ programs/tic_tac_toe.py
@@ -1,4 +1,6 @@
 from itertools import cycle
+import os
+
 class Players:
     def __init__(self, name, solo, multi):
         self.name = input("name")
@@ -30,105 +32,129 @@ class Game:
         self.win = win
         self.tie = tie
         self.winner = ""
+        self.quit = True
+        self.board = Board()
+        
+    def check_taken(self, choice):
+        if str(self.board.moves[int(choice)]) in "XO":
+            print("Position taken try another spot: ")
+            return True
+        else:
+            return False    
 
+
+    
+    def user_inputs(self):
+        print(self.board)
+        curr_turn = next(self.board.player_turn)
+        # Move the line below me into the main game loop and instead
+        choice = "10"
+        while True:
+            try:
+                
+                choice = input(f"{curr_turn} where do you want to move 0-8 or \"Q\" to quit: ")
+                if choice and choice in "0, 1, 2, 3, 4, 5, 6, 7, 8":  
+                    if not self.check_taken(choice):               
+                        self.board.moves[int(choice)] = curr_turn
+                        break
+                elif choice and choice in "Qq":
+                    self.end_game()
+                else:
+                    choice = "10"
+                    print("Invalid Input! Please try again")
+                
+
+              
+            except KeyboardInterrupt:
+                self.end_game()
+                
+            # print(board)   
+
+    def end_game(self):
+        # print("")
+        print("\nThanks for playing!")
+        exit()
+    
     def game_loop(self):
         # We need to make a new board        
-        x1 = Board()
         # We need to display the current board state
         # we need to get the users choice of where to place their move
-        for x in x1.moves:
-            x1 = user_inputs(x1) # need to store value!!!!!!!!!!!!!!!!!!!!!!!!!!
+        for x in self.board.moves:
+            os.system("cls")
+            self.user_inputs()
             # if self.check_win(x1):
-            if self.check_win(x1):
-                print(x1)
+            if self.check_win():
+                print(self.board)
                 print(f"Player {self.winner} Won")
                 break
         # repeat until????
         # print final game state
-        self.win.append(x1)
-        return x1
-        # while True:
-        #     if x1 == win_board:
-        #         print(f"{next} YOU WIN!!!! ")
-        #     else:
-        #         print(f"{next} YOU WIN!!!! ")
-    def check_win(self, board):
+        self.win.append(self.board)
+        return self.board
+
+    def check_win(self):
+        board = self.board
         # Check diagonal
+        # Check for X
+        if board.moves[0] == "X" and board.moves[4] == "X" and board.moves[8] == "X":
+            self.winner = "X"
+            return True
+        elif board.moves[2] == "X" and board.moves[4] == "X" and board.moves[6] == "X":
+            self.winner = "X"
+            return True      
+        # Check for O
+        elif board.moves[0] == "O" and board.moves[4] == "O" and board.moves[8] == "O":
+            self.winner = "O"
+            return True
+        elif board.moves[2] == "O" and board.moves[4] == "O" and board.moves[6] == "O":
+            self.winner = "O"
+            return True
+        # Check Horizontals (3)
             # Check for X
-            if board.moves[0] == "X" and board.moves[4] == "X" and board.moves[8] == "X":
-                self.winner = "X"
-                return True
-            elif board.moves[2] == "X" and board.moves[4] == "X" and board.moves[6] == "X":
-                self.winner = "X"
-                return True      
+        elif board.moves[0] == "X" and board.moves[1] == "X" and board.moves[2] == "X":
+            self.winner = "X"
+            return True            
+        elif board.moves[3] == "X" and board.moves[4] == "X" and board.moves[5] == "X":
+            self.winner = "X"
+            return True
+        elif board.moves[6] == "X" and board.moves[7] == "X" and board.moves[8] == "X":
+            self.winner = "X"
+            return True
             # Check for O
-            elif board.moves[0] == "O" and board.moves[4] == "O" and board.moves[8] == "O":
-                self.winner = "O"
-                return True
-            elif board.moves[2] == "O" and board.moves[4] == "O" and board.moves[6] == "O":
-                self.winner = "O"
-                return True
-            # Check Horizontals (3)
-                # Check for X
-            elif board.moves[0] == "X" and board.moves[1] == "X" and board.moves[2] == "X":
-                self.winner = "X"
-                return True            
-            elif board.moves[3] == "X" and board.moves[4] == "X" and board.moves[5] == "X":
-                self.winner = "X"
-                return True
-            elif board.moves[6] == "X" and board.moves[7] == "X" and board.moves[8] == "X":
-                self.winner = "X"
-                return True
-                # Check for O
-            elif board.moves[0] == "O" and board.moves[1] == "O" and board.moves[2] == "O":
-                self.winner = "O"
-                return True
-            elif board.moves[3] == "O" and board.moves[4] == "O" and board.moves[5] == "O":
-                self.winner = "O"
-                return True
-            elif board.moves[6] == "O" and board.moves[7] == "O" and board.moves[8] == "O":
-                self.winner = "O"
-                return True
+        elif board.moves[0] == "O" and board.moves[1] == "O" and board.moves[2] == "O":
+            self.winner = "O"
+            return True
+        elif board.moves[3] == "O" and board.moves[4] == "O" and board.moves[5] == "O":
+            self.winner = "O"
+            return True
+        elif board.moves[6] == "O" and board.moves[7] == "O" and board.moves[8] == "O":
+            self.winner = "O"
+            return True
         # Check Verticals (3)
-            # Check for X
-            elif board.moves[0] == "X" and board.moves[3] == "X" and board.moves[6] == "X":
-                self.winner = "X"
-                return True
-            elif board.moves[1] == "X" and board.moves[4] == "X" and board.moves[7] == "X":
-                self.winner = "X"
-                return True
-            elif board.moves[2] == "X" and board.moves[5] == "X" and board.moves[8] == "X":
-                self.winner = "X"
-                return True
-            # Check for O
-            elif board.moves[0] == "O" and board.moves[3] == "O" and board.moves[6] == "O":
-                self.winner = "O"
-                return True
-            elif board.moves[1] == "O" and board.moves[4] == "O" and board.moves[7] == "O":
-                self.winner = "O"
-                return True
-            elif board.moves[2] == "O" and board.moves[5] == "O" and board.moves[8] == "O":
-                self.winner = "O"
-                return True
-            else:
-                return False
-def user_inputs(board):
-    print("board object:")
-    print(board)
-    curr_turn = next(board.player_turn)
-    #Move the line below me into the main game loop and instead
-    choice = "10"
-    while True:
-        try:
-            if int(choice) in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
-                print("got a valid input")
-                board.moves[int(choice)] = curr_turn
-                break    
-            choice = input(f"{curr_turn} where do you want to move 0-8: ")
-        except:
-            choice = "10"
-            print("Invalid Input! Please try again")
-        
-    return board # returns board with curent player selection     
+        # Check for X
+        elif board.moves[0] == "X" and board.moves[3] == "X" and board.moves[6] == "X":
+            self.winner = "X"
+            return True
+        elif board.moves[1] == "X" and board.moves[4] == "X" and board.moves[7] == "X":
+            self.winner = "X"
+            return True
+        elif board.moves[2] == "X" and board.moves[5] == "X" and board.moves[8] == "X":
+            self.winner = "X"
+            return True
+        # Check for O
+        elif board.moves[0] == "O" and board.moves[3] == "O" and board.moves[6] == "O":
+            self.winner = "O"
+            return True
+        elif board.moves[1] == "O" and board.moves[4] == "O" and board.moves[7] == "O":
+            self.winner = "O"
+            return True
+        elif board.moves[2] == "O" and board.moves[5] == "O" and board.moves[8] == "O":
+            self.winner = "O"
+            return True
+        else:
+            return False
+
+
+
 game = Game()
 win = game.game_loop()
---
+++ programs/tic_tac_toeEmojii.py
@@ -0,0 +1,197 @@
+from itertools import cycle
+import os
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+    
+    def move():   
+        pass
+        # TODO call game logic for non-bot to take a turn AKA getting input
+        
+    
+class Bot(Player):
+    def __init__(self, name):
+        super().__init__(name)
+    # CREATE BOT LOGIC
+
+
+class Board:
+    def __init__(self):
+        self.board = ""
+        self.moves = self.set_empty_moves()
+
+        # self.win_board() = ########################
+    def __str__(self):
+        return self.get_board()
+    def __repr__(self):
+        return "this is a board object"    
+    
+    def set_empty_moves(self):
+        temp_list = []
+        temp_list.append("0️⃣")
+        temp_list.append("1️⃣")
+        temp_list.append("2️⃣")
+        temp_list.append("3️⃣")
+        temp_list.append("4️⃣")
+        temp_list.append("5️⃣")
+        temp_list.append("6️⃣")
+        temp_list.append("7️⃣")
+        temp_list.append("8️⃣")
+        return temp_list
+ 
+    def get_board(self):
+        
+        self.board = f' ____________________\n|      |      |      |\n|  {self.moves[0]}   |  {self.moves[1]}   |  {self.moves[2]}   |\n|______|______|______|\n|      |      |      |\n|  {self.moves[3]}   |  {self.moves[4]}   |  {self.moves[5]}   |\n|______|______|______|\n|      |      |      |\n|  {self.moves[6]}   |  {self.moves[7]}   |  {self.moves[8]}   |\n|______|______|______|'
+        return self.board
+    
+class Game:
+    def __init__(self, win = [], tie = []):
+        self.turns = ["⛌", "◯"] # TODO see line 61        
+        self.players = self.set_players()
+        self.quitting_chars = ["Q","quit"]
+        self.win = win
+        self.tie = tie
+        self.winner = ""
+        self.quit = True
+        self.board = Board()
+        self.player_turn = self.get_player_iterator()
+    def get_player_iterator(self):
+        # TODO:Create an array with an X player and an O player with the names of turns[0] and turns[1]
+        # TODO: return cycle of that array
+        return cycle(self.turns)
+    def set_players(self):
+        # TODO: finish writing this to return the player array
+        player_1 = Player(self.turns[0]) # need to designate name using turns[0]
+        player_2 = Player(self.turns[1])# need to designate name using turns[1]
+        return [player_1, player_2]    
+    def check_taken(self, choice):
+        if str(self.board.moves[int(choice)]) in self.turns:
+            print("Position taken try another spot: ")
+            return True
+        else:
+            return False    
+
+
+    
+    def user_inputs(self):
+        print(self.board)
+        curr_turn = next(self.player_turn)
+        # Move the line below me into the main game loop and instead
+        choice = "10"
+        while True:
+            try:
+                quit_string = " or ".join([", ".join(self.quitting_chars[:-1]),self.quitting_chars[-1]])
+                # quit_string = " or ".join(["Q, q",self.quitting_chars[-1]])
+                # quit_string = " or ".join(["Q, q","exit"])
+                # quit_string = "Q, q" + " or " + "exit"
+
+                choice = input(f'{curr_turn} where do you want to move 0-8 or {quit_string} to quit: ')
+                if choice and choice in "0, 1, 2, 3, 4, 5, 6, 7, 8":  
+                    if not self.check_taken(choice):               
+                        self.board.moves[int(choice)] = curr_turn
+                        break
+                elif choice and choice.upper() in self.quitting_chars:
+                    self.end_game()
+                else:
+                    choice = "10"
+                    print("Invalid Input! Please try again")
+                
+
+              
+            except KeyboardInterrupt:
+                self.end_game()
+                
+            # print(board)   
+
+    def end_game(self):
+        # print("")
+        print("\nThanks for playing!")
+        exit()
+    
+    def game_loop(self):
+        # We need to make a new board        
+        # We need to display the current board state
+        # we need to get the users choice of where to place their move
+        for x in self.board.moves:
+            os.system("cls")
+            self.user_inputs()
+            # if self.check_win(x1):
+            if self.check_win():
+                print(self.board)
+                print(f"Player {self.winner} Won")
+                break
+        # repeat until????
+        # print final game state
+        self.win.append(self.board)
+        return self.board
+
+    def check_win(self):
+        board = self.board
+        x_move = self.turns[0]
+        o_move = self.turns[1]
+        # Check diagonal
+        # Check for X
+        if board.moves[0] == x_move and board.moves[4] == x_move and board.moves[8] == x_move:
+            self.winner = x_move
+            return True
+        elif board.moves[2] == x_move and board.moves[4] == x_move and board.moves[6] == x_move:
+            self.winner = x_move
+            return True      
+        # Check for O
+        elif board.moves[0] == o_move and board.moves[4] == o_move and board.moves[8] == o_move:
+            self.winner = o_move
+            return True
+        elif board.moves[2] == o_move and board.moves[4] == o_move and board.moves[6] == o_move:
+            self.winner = o_move
+            return True
+        # Check Horizontals (3)
+            # Check for X
+        elif board.moves[0] == x_move and board.moves[1] == x_move and board.moves[2] == x_move:
+            self.winner = x_move
+            return True            
+        elif board.moves[3] == x_move and board.moves[4] == x_move and board.moves[5] == x_move:
+            self.winner = x_move
+            return True
+        elif board.moves[6] == x_move and board.moves[7] == x_move and board.moves[8] == x_move:
+            self.winner = x_move
+            return True
+            # Check for O
+        elif board.moves[0] == o_move and board.moves[1] == o_move and board.moves[2] == o_move:
+            self.winner = o_move
+            return True
+        elif board.moves[3] == o_move and board.moves[4] == o_move and board.moves[5] == o_move:
+            self.winner = o_move
+            return True
+        elif board.moves[6] == o_move and board.moves[7] == o_move and board.moves[8] == o_move:
+            self.winner = o_move
+            return True
+        # Check Verticals (3)
+        # Check for X
+        elif board.moves[0] == x_move and board.moves[3] == x_move and board.moves[6] == x_move:
+            self.winner = x_move
+            return True
+        elif board.moves[1] == x_move and board.moves[4] == x_move and board.moves[7] == x_move:
+            self.winner = x_move
+            return True
+        elif board.moves[2] == x_move and board.moves[5] == x_move and board.moves[8] == x_move:
+            self.winner = x_move
+            return True
+        # Check for O
+        elif board.moves[0] == o_move and board.moves[3] == o_move and board.moves[6] == o_move:
+            self.winner = o_move
+            return True
+        elif board.moves[1] == o_move and board.moves[4] == o_move and board.moves[7] == o_move:
+            self.winner = o_move
+            return True
+        elif board.moves[2] == o_move and board.moves[5] == o_move and board.moves[8] == o_move:
+            self.winner = o_move
+            return True
+        else:
+            print("Draw: ")
+            return False
+
+
+
+game = Game()
+win = game.game_loop()
